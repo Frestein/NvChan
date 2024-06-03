9b0bbf2275f4ddded290e575d3053465483efbd9
Frestein <Frestein@tuta.io>
2024-06-03 17:19:24 +0500

    fix(nvim-silicon): correct config

--- lua/configs/silicon.lua
+++ lua/configs/silicon.lua
@@ -1,12 +1,15 @@
 local options = {
-  font = "JetBrains Mono=34;Noto Color Emoji=34",
+  font = "JetBrains Mono=34",
   to_clipboard = true,
   theme = "Nord",
   pad_horiz = 50,
   pad_vert = 40,
+  language = function()
+    return vim.bo.filetype
+  end,
   window_title = function()
     return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
   end,
 }
 
-require("silicon").setup(options)
+require("nvim-silicon").setup(options)
