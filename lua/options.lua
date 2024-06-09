25c52345755b3ed8226911e09a89901045309d63
Frestein <Frestein@tuta.io>
2024-06-10 01:22:12 +0500

    feat(cursor): add blinking cursor

--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,7 @@ require "nvchad.options"
 local o = vim.o
 local autocmd = vim.api.nvim_create_autocmd
 
+o.guicursor="n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 o.relativenumber = true
 o.swapfile = false
 o.scrolloff = 5
