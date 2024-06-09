e5615bffb175f8e40ff583e886018129994e7c15
Frestein <Frestein@tuta.io>
2024-06-10 01:18:38 +0500

    refactor(lspconfig): clean lspconfig syntax

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,41 +4,33 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 local servers = {
-  "html",
-  "cssls",
-  "bashls",
-  "ruff",
-  "yamlls",
-  "clangd",
-  "kotlin_language_server",
-}
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
-
-lspconfig.pyright.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", "requirements.txt"),
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        diagnosticMode = "workspace",
-        typeCheckingMode = "off",
+  html = {},
+  awk_ls = {},
+  bashls = {},
+  yamlls = {},
+  clangd = {},
+  kotlin_language_server = {},
+  pyright = {
+    root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", "requirements.txt"),
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          diagnosticMode = "workspace",
+          typeCheckingMode = "off",
+        },
       },
     },
   },
+  ruff = {
+    root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", "requirements.txt"),
+  },
 }
 
+for name, opts in pairs(servers) do
+  opts.on_init = on_init
+  opts.on_attach = on_attach
+  opts.capabilities = capabilities
 
-lspconfig.ruff.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = lspconfig.util.root_pattern(".git", "setup.py", "pyproject.toml", "requirements.txt"),
-}
+  require("lspconfig")[name].setup(opts)
+end
