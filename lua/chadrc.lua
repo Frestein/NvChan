71faedddf3fab44f48c8d70a051a245cda4b740a
Frestein <Frestein@tuta.io>
2024-07-31 21:30:32 +0500

    feat(transparency): override navic and other highlights

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -35,6 +35,135 @@ M.base46 = {
     ["TbFill"] = {
       bg = "NONE",
     },
+    ["TbBufOn"] = {
+      bg = "NONE",
+    },
+    ["TbBufOnClose"] = {
+      bg = "NONE",
+    },
+    ["TbBufOff"] = {
+      bg = "NONE",
+    },
+    ["TbBufOffClose"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsColor"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsInterface"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsFile"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsClass"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsUnit"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsEnum"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsProperty"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsModule"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsFolder"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsConstructor"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsMethod"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsKeyword"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsType"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsStructure"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsText"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsSnippet"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsVariable"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsField"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsIdentifier"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsFunction"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsConstant"] = {
+      bg = "NONE",
+    },
+    ["NavicSeparator"] = {
+      bg = "NONE",
+    },
+    ["NavicText"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsNull"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsKey"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsObject"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsArray"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsBoolean"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsNumber"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsString"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsPackage"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsNamespace"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsTypeParameter"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsOperator"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsEvent"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsValue"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsStruct"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsEnumMember"] = {
+      bg = "NONE",
+    },
+    ["NavicIconsReference"] = {
+      bg = "NONE",
+    },
   },
 
   hl_add = {
@@ -44,6 +173,16 @@ M.base46 = {
     },
     ["WinBarNC"] = {
       fg = "light_grey",
+      bg = "NONE",
+    },
+    ["StatusLineNC"] = {
+      fg = "light_grey",
+      bg = "black",
+    },
+    ["NvimTreeEndOfBuffer"] = {
+      bg = "NONE",
+    },
+    ["NvimTreeWinSeparator"] = {
       bg = "NONE",
     },
   },
