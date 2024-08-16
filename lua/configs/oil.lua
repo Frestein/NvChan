5be62678c623cc61798c30984c06ff1f93d08c5d
Frestein <Frestein@tuta.io>
2024-08-16 15:58:18 +0500

    refactor(oil): use new method for handling git-ignored files
    - Implemented a new approach for processing git-ignored files using a metatable and `vim.system`.

--- lua/configs/oil.lua
+++ lua/configs/oil.lua
@@ -1,79 +1,38 @@
--- @class GitCache
--- @field func function
--- @field results table
-local GitCache = {}
+local git_ignored = setmetatable({}, {
+  __index = function(self, key)
+    local proc = vim.system({ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" }, {
+      cwd = key,
+      text = true,
+    })
+    local result = proc:wait()
+    local ret = {}
+    if result.code == 0 then
+      for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+        -- Remove trailing slash
+        line = line:gsub("/$", "")
+        table.insert(ret, line)
+      end
+    end
 
--- @param func function
--- @return GitCache
-function GitCache:new(func)
-  local tbl = {
-    func = func,
-    results = {},
-  }
-  self.__index = self
-  setmetatable(tbl, self)
-  return tbl
-end
-
--- @param dir string
-function GitCache:_call(dir)
-  local cmd =
-    string.format('git -C %s ls-files --ignored --exclude-standard --others --directory | grep -v "/.*/"', dir)
-
-  local handle = io.popen(cmd)
-  if handle == nil then
-    return
-  end
-
-  local output = handle:read "*a"
-  handle:close()
-
-  -- Clean the output a bit
-  local lines = vim.split(output, "\n")
-  local ignored_files = {}
-  for _, line in ipairs(lines) do
-    -- Remove trailing slash
-    line = line:gsub("/$", "")
-    table.insert(ignored_files, line)
-  end
-  table.insert(ignored_files, ".git")
-  table.insert(ignored_files, "..")
-
-  self.results[dir] = ignored_files
-  self.checked = true
-end
-
--- @param dir string
--- @return table
-function GitCache:call(dir)
-  if self.results[dir] == nil then
-    -- Reset cache, we don't care about old dir results
-    self.results = {}
-
-    self:_call(dir)
-  end
-  return self.results[dir]
-end
-
-local cache = GitCache:new(get_ignored)
-
-local function get_git_ignored_files_in(dir)
-  local found = vim.fs.find(".git", {
-    upward = true,
-    path = dir,
-  })
-  if #found == 0 then
-    return {}
-  end
-
-  return cache:call(dir)
-end
+    rawset(self, key, ret)
+    return ret
+  end,
+})
 
 local options = {
   view_options = {
-    is_hidden_file = function(name, bufnr)
-      local ignored_files = get_git_ignored_files_in(require("oil").get_current_dir())
-      return vim.tbl_contains(ignored_files, name) or vim.startswith(name, ".")
+    is_hidden_file = function(name, _)
+      -- dotfiles are always considered hidden
+      if vim.startswith(name, ".") then
+        return true
+      end
+      local dir = require("oil").get_current_dir()
+      -- if no local directory (e.g. for ssh connections), always show
+      if not dir then
+        return false
+      end
+      -- Check if file is gitignored
+      return vim.list_contains(git_ignored[dir], name)
     end,
   },
 }
