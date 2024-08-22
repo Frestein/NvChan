6d681cc1e9656084f572f53622be6eb5c530ae61
Frestein <Frestein@tuta.io>
2024-08-22 20:11:56 +0500

    feat(conform): leave visual mode after range format

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -121,8 +121,15 @@ map("n", "<leader>fl", oil.toggle_float, { desc = "file open parent directory (f
 -- Conform {{{
 
 map({ "n", "v" }, "<leader>fc", function()
-  conform.format { async = true }
-end, { desc = "file format buffer" })
+  conform.format({ async = true }, function(err)
+    if not err then
+      local mode = vim.api.nvim_get_mode().mode
+      if vim.startswith(string.lower(mode), "v") then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+      end
+    end
+  end)
+end, { desc = "file format code" })
 
 -- }}}
 -- Lazy {{{
