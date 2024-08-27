f844c4b9d8002d5591d7df91e192a48d0ce7f076
Frestein <Frestein@tuta.io>
2024-08-28 01:42:16 +0500

    feat(mason): add MasonInstallAll command

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,6 +18,24 @@ return {
     config = function(_, opts)
       require("mason").setup(opts)
       require "plugins.mappings.mason-keys"
+
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        if opts.ensure_installed and #opts.ensure_installed > 0 then
+          vim.cmd "Mason"
+          local mr = require "mason-registry"
+
+          mr.refresh(function()
+            for _, tool in ipairs(opts.ensure_installed) do
+              local p = mr.get_package(tool)
+              if not p:is_installed() then
+                p:install()
+              end
+            end
+          end)
+        end
+      end, {})
+
+      vim.g.mason_binaries_list = opts.ensure_installed
     end,
   },
 
