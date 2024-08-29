1519dfa1674d479c53ccc0260138e906fcf6d12a
Frestein <Frestein@tuta.io>
2024-08-29 22:17:38 +0500

    feat(oil): improve git integrated hidden files logic

--- lua/plugins/options/oil-opts.lua
+++ lua/plugins/options/oil-opts.lua
@@ -1,26 +1,5 @@
 local detail = false
 
-local git_ignored = setmetatable({}, {
-  __index = function(self, key)
-    local proc = vim.system({ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" }, {
-      cwd = key,
-      text = true,
-    })
-    local result = proc:wait()
-    local ret = {}
-    if result.code == 0 then
-      for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
-        -- Remove trailing slash
-        line = line:gsub("/$", "")
-        table.insert(ret, line)
-      end
-    end
-
-    rawset(self, key, ret)
-    return ret
-  end,
-})
-
 local options = {
   delete_to_trash = true,
   skip_confirm_for_simple_edits = true,
@@ -36,21 +15,6 @@ local options = {
         end
       end,
     },
-  },
-  view_options = {
-    is_hidden_file = function(name, _)
-      -- dotfiles are always considered hidden
-      if vim.startswith(name, ".") then
-        return true
-      end
-      local dir = require("oil").get_current_dir()
-      -- if no local directory (e.g. for ssh connections), always show
-      if not dir then
-        return false
-      end
-      -- Check if file is gitignored
-      return vim.list_contains(git_ignored[dir], name)
-    end,
   },
   git = {
     add = function(path)
