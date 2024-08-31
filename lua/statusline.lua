c01fd25d764ac6aeb4d8a93db3620008f1199653
Frestein <Frestein@tuta.io>
2024-08-31 18:14:35 +0500

    perf(trouble): initialize trouble statusline only when LSP is attached

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -2,20 +2,22 @@ M = {}
 
 local lsp_attached = false
 local highlight_set = false
+local trouble, symbols
 
 function M.set_lsp_attached(value)
   lsp_attached = value
+  if lsp_attached then
+    trouble = require "trouble"
+    symbols = trouble.statusline {
+      mode = "lsp_document_symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      hl_group = "lualine_c_normal",
+    }
+  end
 end
-
-local trouble = require "trouble"
-local symbols = trouble.statusline {
-  mode = "lsp_document_symbols",
-  groups = {},
-  title = false,
-  filter = { range = true },
-  format = "{kind_icon}{symbol.name:Normal}",
-  hl_group = "lualine_c_normal",
-}
 
 local function set_trouble_highlights()
   if not highlight_set then
@@ -29,9 +31,7 @@ end
 function M.trouble()
   if not lsp_attached then
     return ""
-  end
-
-  if symbols.has() then
+  elseif symbols and symbols.has() then
     local statusline_symbols = symbols.get()
     set_trouble_highlights()
     return statusline_symbols
