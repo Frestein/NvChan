b93605d5cf534487bc11fe96c4d5031c8cc94428
Frestein <Frestein@tuta.io>
2024-09-02 01:58:23 +0500

    chore(mason): move packages to system package manager

--- lua/plugins/options/mason-opts.lua
+++ lua/plugins/options/mason-opts.lua
@@ -1,30 +1,13 @@
 local options = require "nvchad.configs.mason"
 
 options.ensure_installed = {
-  "lua-language-server",
-  "stylua",
   "html-lsp",
   "css-lsp",
-  "prettier",
-  "pyright",
-  "black",
-  "ruff",
-  "yaml-language-server",
-  "yamllint",
-  "ansible-language-server",
-  "ansible-lint",
   "dockerfile-language-server",
   "hadolint",
   "clangd",
   "clang-format",
-  "shfmt",
-  "shellharden",
-  "shellcheck",
-  "bash-language-server",
   "hyprls",
-  "taplo",
-  "selene",
-  "marksman",
 }
 
 return options
