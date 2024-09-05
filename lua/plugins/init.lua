f543f9c2d904d1ec0f4747313db6c59f9a135746
Frestein <Frestein@tuta.io>
2024-09-05 23:49:41 +0500

    feat(plugins): add go.nvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -183,6 +183,18 @@ return {
   },
 
   {
+    "ray-x/go.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ft = { "go", "gomod" },
+    config = function(_, opts)
+      require("go").setup(opts)
+    end,
+  },
+
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = "nvim-lua/plenary.nvim",
     keys = {
