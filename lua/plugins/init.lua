6daddb6cfeebcf9c687a7f86cf41fab9018620d8
Frestein <Frestein@tuta.io>
2024-09-06 00:44:23 +0500

    refactor(mason): extract mason configuration to separate file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,27 +16,9 @@ return {
       "MasonUninstallAll",
     },
     opts = require "plugins.options.mason-opts",
-    config = function(_, opts)
-      require("mason").setup(opts)
+    config = function()
+      require "plugins.configs.mason-conf"
       require "plugins.mappings.mason-keys"
-
-      vim.api.nvim_create_user_command("MasonInstallAll", function()
-        if opts.ensure_installed and #opts.ensure_installed > 0 then
-          vim.cmd "Mason"
-          local mr = require "mason-registry"
-
-          mr.refresh(function()
-            for _, tool in ipairs(opts.ensure_installed) do
-              local p = mr.get_package(tool)
-              if not p:is_installed() then
-                p:install()
-              end
-            end
-          end)
-        end
-      end, {})
-
-      vim.g.mason_binaries_list = opts.ensure_installed
     end,
   },
 
