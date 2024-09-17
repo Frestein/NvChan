df6ba2b548c8c18b272c198f60f7301a1c5bed1d
Frestein <Frestein@tuta.io>
2024-09-17 09:41:17 +0500

    style(mini-bufremove-keys): format file

--- lua/plugins/mappings/mini-bufremove-keys.lua
+++ lua/plugins/mappings/mini-bufremove-keys.lua
@@ -1,16 +1,18 @@
 local set_keymaps = require("utils").set_keymaps
 local map = require("langmapper").map
 
-local bufremove = require"mini.bufremove"
+local bufremove = require "mini.bufremove"
 
+-- stylua: ignore
 local keymaps = {
-  ["<leader>bd"] = { func = function ()
-    bufremove.delete(0, false)
-  end, desc = "buffer delete buffer" },
-  ["<leader>bD"] = { func = function ()
-    bufremove.delete(0, true)
-  end, desc = "buffer delete buffer (force)" },
+  ["<leader>bd"] = {
+    func = function() bufremove.delete(0, false) end,
+    desc = "buffer delete buffer",
+  },
+  ["<leader>bD"] = {
+    func = function() bufremove.delete(0, true) end,
+    desc = "buffer delete buffer (force)",
+  },
 }
 
 set_keymaps(map, keymaps)
-
