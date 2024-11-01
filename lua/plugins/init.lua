44e564ef2f30785950534fedd2a76de03392a26b
Frestein <Frestein@tuta.io>
2024-11-01 23:00:52 +0500

    chore(which-key): migrate configuration from NvChad

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -778,6 +778,12 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+    cmd = "WhichKey",
+    opts = function()
+      dofile(vim.g.base46_cache .. "whichkey")
+      return {}
+    end,
   },
 
   {
