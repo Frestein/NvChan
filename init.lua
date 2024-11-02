f3eee6d18a40b4f7951e7b4594aa01dbafdba140
Frestein <Frestein@tuta.io>
2024-11-02 09:14:05 +0500

    refactor(lazy): improve and clean up lazy.nvim setup

--- init.lua
+++ init.lua
@@ -2,40 +2,17 @@ vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+-- Load plugins
+require "plugins.configs.lazy-conf"
 
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-local lazy_options = require "plugins.options.lazy-opts"
-
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
-
-  { import = "plugins" },
-}, lazy_options)
-
--- load theme
+-- Load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
+-- Load options
 require "options"
 require "autocmds"
-require "nvchad.autocmds"
+require "mappings"
 
-vim.schedule(function()
-  require "mappings"
-end)
-
+-- Automap keybindings
 require("langmapper").automapping { global = true, buffer = true }
