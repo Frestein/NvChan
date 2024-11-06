acfa87d48caa2f0951ade4da191e2e18bbc96407
Frestein <Frestein@tuta.io>
2024-11-06 13:35:29 +0500

    feat(symbol-usage): clean up

--- lua/plugins/configs/zen-mode-conf.lua
+++ lua/plugins/configs/zen-mode-conf.lua
@@ -1,23 +1,13 @@
 local incline = require "incline"
 local opts = require "plugins.options.zen-mode-opts"
-local sbl_us = require "symbol-usage"
-local sbl_us_opts = require "plugins.options.symbol-usage-opts"
 local zen_mode = require "zen-mode"
 
-opts.on_open = function(win)
+opts.on_open = function()
 	incline.disable()
-
-	sbl_us_opts.opts.text_format = sbl_us_opts.plain_text
-	sbl_us.setup(sbl_us_opts.opts)
-	sbl_us.refresh()
 end
 
 opts.on_close = function()
 	incline.enable()
-
-	sbl_us_opts.opts.text_format = sbl_us_opts.plain_text_symbol
-	sbl_us.setup(sbl_us_opts.opts)
-	sbl_us.refresh()
 end
 
 zen_mode.setup(opts)
--- lua/plugins/options/symbol-usage-opts.lua
+++ lua/plugins/options/symbol-usage-opts.lua
@@ -1,29 +1,6 @@
 local M = {}
 
-M.plain_text = function(symbol)
-	local fragments = {}
-
-	-- Indicator that shows if there are any other symbols in the same line
-	local stacked_functions = symbol.stacked_count > 0 and (" | +%s"):format(symbol.stacked_count) or ""
-
-	if symbol.references then
-		local usage = symbol.references <= 1 and "usage" or "usages"
-		local num = symbol.references == 0 and "no" or symbol.references
-		table.insert(fragments, ("%s %s"):format(num, usage))
-	end
-
-	if symbol.definition then
-		table.insert(fragments, symbol.definition .. " defs")
-	end
-
-	if symbol.implementation then
-		table.insert(fragments, symbol.implementation .. " impls")
-	end
-
-	return table.concat(fragments, ", ") .. stacked_functions
-end
-
-M.plain_text_symbol = function(symbol)
+local function plain_text_symbol(symbol)
 	local res = {}
 
 	-- Indicator that shows if there are any other symbols in the same line
@@ -64,10 +41,21 @@ M.plain_text_symbol = function(symbol)
 end
 
 M.opts = {
+	---@type 'above'|'end_of_line'|'textwidth'|'signcolumn' `above` by default
 	vt_position = "end_of_line",
+	---The function can return a string to which the highlighting group from `opts.hl` is applied.
+	---Alternatively, it can return a table of tuples of the form `{ { text, hl_group }, ... }`` - in this case the specified groups will be applied.
+	---If `vt_position` is 'signcolumn', then only a 1-2 length string or a `{{ icon, hl_group }}` table is expected.
+	---See `#format-text-examples`
+	---@type function(symbol: Symbol): string|table Symbol{ definition = integer|nil, implementation = integer|nil, references = integer|nil, stacked_count = integer, stacked_symbols = table<SymbolId, Symbol> }
+	text_format = plain_text_symbol,
+	---Text to display when request is pending. If `false`, extmark will not be
+	---created until the request is finished. Recommended to use with `above`
+	---vt_position to avoid "jumping lines".
+	---@type string|table|false
+	request_pending_text = false,
 	definition = { enabled = false },
 	implementation = { enabled = false },
-	text_format = M.plain_text_symbol,
 }
 
 return M
