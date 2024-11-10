5c63959dd0b110f7564bddc7f0ae085abf666dba
Frestein <Frestein@tuta.io>
2024-11-10 17:56:01 +0500

    style: format code

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -209,11 +209,13 @@ end
 
 -- Toggle options
 Snacks.toggle.option("spell", { name = "Spelling" }):map "<leader>us"
-Snacks.toggle.option("wrap", {name = "Wrap"}):map("<leader>uw")
-Snacks.toggle.line_number():map("<leader>ul")
+Snacks.toggle.option("wrap", { name = "Wrap" }):map "<leader>uw"
+Snacks.toggle.line_number():map "<leader>ul"
 Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map "<leader>uL"
-Snacks.toggle.diagnostics():map("<leader>ud")
-Snacks.toggle.option("conceallevel", {off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2}):map("<leader>uc")
+Snacks.toggle.diagnostics():map "<leader>ud"
+Snacks.toggle
+	.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+	:map "<leader>uc"
 if vim.lsp.inlay_hint then
-  Snacks.toggle.inlay_hints():map("<leader>uh")
+	Snacks.toggle.inlay_hints():map "<leader>uh"
 end
