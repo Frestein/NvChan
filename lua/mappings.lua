8105a91181e1e7cc2b930649c7eb4274a2bb2da0
Frestein <Frestein@tuta.io>
2024-11-11 02:56:04 +0500

    feat(snacks): add pretty debug and lazy loading toggled keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -242,16 +242,3 @@ for _, mapping in ipairs(mappings) do
 	local mode, lhs, rhs, opts = table.unpack(mapping)
 	map(mode, lhs, rhs, opts)
 end
-
--- Toggle options
-Snacks.toggle.option("spell", { name = "Spelling" }):map "<leader>us"
-Snacks.toggle.option("wrap", { name = "Wrap" }):map "<leader>uw"
-Snacks.toggle.line_number():map "<leader>ul"
-Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map "<leader>uL"
-Snacks.toggle.diagnostics():map "<leader>ud"
-Snacks.toggle
-	.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-	:map "<leader>uc"
-if vim.lsp.inlay_hint then
-	Snacks.toggle.inlay_hints():map "<leader>uh"
-end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,7 +31,33 @@ return {
 			return require "plugins.options.snacks-opts"
 		end,
 		init = function()
-			_G.Snacks = require "snacks"
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					_G.Snacks = require "snacks"
+					-- Setup some globals for debugging (lazy-loaded)
+					_G.dd = function(...)
+						Snacks.debug.inspect(...)
+					end
+					_G.bt = function()
+						Snacks.debug.backtrace()
+					end
+					vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+					-- Toggle options
+					Snacks.toggle.option("spell", { name = "Spelling" }):map "<leader>us"
+					Snacks.toggle.option("wrap", { name = "Wrap" }):map "<leader>uw"
+					Snacks.toggle.line_number():map "<leader>ul"
+					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map "<leader>uL"
+					Snacks.toggle.diagnostics():map "<leader>ud"
+					Snacks.toggle
+						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+						:map "<leader>uc"
+					if vim.lsp.inlay_hint then
+						Snacks.toggle.inlay_hints():map "<leader>uh"
+					end
+				end,
+			})
 		end,
 	},
 
