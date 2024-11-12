b0769e1f06d9fad472a0f01d9ad6234f11b9a6ce
Frestein <Frestein@tuta.io>
2024-11-12 22:12:29 +0500

    fix(zen-mode): resolve the issue with incline

--- lua/plugins/configs/zen-mode-conf.lua
+++ lua/plugins/configs/zen-mode-conf.lua
@@ -1,13 +1,15 @@
-local incline = require "incline"
 local opts = require "plugins.options.zen-mode-opts"
 local zen_mode = require "zen-mode"
 
-opts.on_open = function()
-	incline.disable()
-end
+local ok, incline = pcall(require, "incline")
+if ok then
+	opts.on_open = function()
+		incline.disable()
+	end
 
-opts.on_close = function()
-	incline.enable()
+	opts.on_close = function()
+		incline.enable()
+	end
 end
 
 zen_mode.setup(opts)
