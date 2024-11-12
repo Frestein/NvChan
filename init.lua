3cf5dedd407e3ffbdeed46785c16a05a31ddb960
Frestein <Frestein@tuta.io>
2024-11-12 10:01:57 +0500

    fix: resolve the issue with langmapper

--- init.lua
+++ init.lua
@@ -2,8 +2,9 @@ vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Load compatibility
+-- Load compatibility and default options
 require "compat"
+require "options"
 
 -- Load plugins
 require "plugins.configs.lazy-conf"
@@ -13,7 +14,6 @@ dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
 -- Load options
-require "options"
 require "autocmds"
 require "mappings"
 
