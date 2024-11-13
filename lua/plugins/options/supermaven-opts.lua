64824d1ad99b8a82505df6b47b24e0415cd3187c
Frestein <Frestein@tuta.io>
2024-11-13 22:19:36 +0500

    fix(supermaven): resolve the issue with accept suggestion

--- lua/plugins/options/supermaven-opts.lua
+++ lua/plugins/options/supermaven-opts.lua
@@ -2,7 +2,7 @@ local colors = require("utils").get_base46_colors()
 
 local opts = {
 	keymaps = {
-		accept_suggestion = "<C-f>",
+		accept_suggestion = "<C-\\>",
 		accept_word = "<C-w>",
 		clear_suggestion = "<C-c>",
 	},
