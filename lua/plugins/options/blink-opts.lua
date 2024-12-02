e4483f37a7949d372127bbd08bea274356ea19db
Frestein <Frestein@tuta.io>
2024-12-03 00:40:05 +0500

    fix(blink): update config to reflect breaking changes

--- lua/plugins/options/blink-opts.lua
+++ lua/plugins/options/blink-opts.lua
@@ -3,8 +3,9 @@ dofile(vim.g.base46_cache .. "cmp")
 ---@module 'blink.cmp'
 ---@type blink.cmp.Config | { sources: { compat: string[] } }
 local opts = {
-	highlight = {
+	appearance = {
 		use_nvim_cmp_as_default = true,
+		nerd_font_variant = "mono",
 	},
 	completion = {
 		menu = {
